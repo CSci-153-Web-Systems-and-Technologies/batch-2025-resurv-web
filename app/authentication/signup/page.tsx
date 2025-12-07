76ee1f72d5862aa22bdf7a25b67bf46caeb28a81
Gabriel Mosquera <mosqueragabriel99@gmail.com>
2025-12-07 16:49:58 +0800

    Apply styling to Signup Page

--- app/authentication/signup/page.tsx
+++ app/authentication/signup/page.tsx
@@ -6,16 +6,16 @@ export default function SignupPage() {
   return (
     <div className="grid min-h-svh lg:grid-cols-2 bg-[#CEDBEE]">
       <div className="flex flex-col gap-4 p-6 md:p-10">
-        <div className="flex justify-center gap-2 md:justify-start">
+        <div className="flex justify-center gap-2 md:justify-start text-[#556378] ">
           <a href="#" className=" font-extrabold tracking-tight flex items-center gap-2">
-            <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+            <div className=" text-[#CEDBEE] flex size-6 items-center justify-center rounded-md bg-[#556378] ">
               <CalendarFold className="size-4" />
             </div>
             Resurv
           </a>
         </div>
         <div className="flex flex-1 items-center justify-center">
-          <div className="w-full max-w-xs">
+          <div className="w-full max-w-xs text-[#556378]">
             <SignupForm />
           </div>
         </div>
--- components/signup-form.tsx
+++ components/signup-form.tsx
@@ -1,5 +1,7 @@
+"use client"
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
+import { useState } from "react"
 import {
   Field,
   FieldDescription,
@@ -13,41 +15,37 @@ export function SignupForm({
   className,
   ...props
 }: React.ComponentProps<"form">) {
+  const [isLoading, setIsLoading] = useState(false)
   return (
     <form className={cn("flex flex-col gap-6", className)} {...props}>
       <FieldGroup>
         <div className="flex flex-col items-center gap-1 text-center">
-          <h1 className="text-2xl font-bold">Create your account</h1>
-          <p className="text-muted-foreground text-sm text-balance">
-            Fill in the form below to create your account
-          </p>
+          <h1 className="text-3xl font-bold">Create your account</h1>
         </div>
         <Field>
           <FieldLabel htmlFor="name">Full Name</FieldLabel>
-          <Input id="name" type="text" placeholder="John Doe" required />
+          <Input id="name" type="text" placeholder="John Doe"  className="border border-[#556378]" required />
         </Field>
         <Field>
           <FieldLabel htmlFor="email">Email</FieldLabel>
-          <Input id="email" type="email" placeholder="m@example.com" required />
-          <FieldDescription>
-            We&apos;ll use this to contact you. We will not share your email
-            with anyone else.
-          </FieldDescription>
+          <Input id="email" type="email" placeholder="m@example.com"  className="border border-[#556378]" required />
         </Field>
         <Field>
           <FieldLabel htmlFor="password">Password</FieldLabel>
-          <Input id="password" type="password" required />
+          <Input id="password" type="password"  className="border border-[#556378]" required />
           <FieldDescription>
             Must be at least 8 characters long.
           </FieldDescription>
         </Field>
         <Field>
           <FieldLabel htmlFor="confirm-password">Confirm Password</FieldLabel>
-          <Input id="confirm-password" type="password" required />
+          <Input id="confirm-password" type="password"  className="border border-[#556378]" required />
           <FieldDescription>Please confirm your password.</FieldDescription>
         </Field>
         <Field>
-          <Button type="submit">Create Account</Button>
+          <Button type="submit" className="bg-[#556378] w-full cursor-pointer" disabled={isLoading}>
+             {isLoading ? "Creating Account..." : "Create Account"}
+          </Button>
         </Field>
         <FieldSeparator>Or continue with</FieldSeparator>
         <Field>
