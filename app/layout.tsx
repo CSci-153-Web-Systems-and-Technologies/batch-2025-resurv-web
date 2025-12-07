8a88270e87b76a2fa02b7e63a9e4fe101a482987
Gabriel Mosquera <mosqueragabriel99@gmail.com>
2025-12-07 16:22:51 +0800

    Change metadata title to match project title

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Unibooked",
+  title: "Resurv",
   description: "Facility Reservation System",
 };
 
