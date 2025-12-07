40a7c7f36b95325ce36fd04f5a3dae4a6d3f2f7d
Gabriel Mosquera <mosqueragabriel99@gmail.com>
2025-12-07 22:33:23 +0800

    Add a calendar to Reservation Page
    Allows users to view bookings on a calendar

--- app/authentication/reservation/page.tsx
+++ app/authentication/reservation/page.tsx
@@ -1,6 +1,12 @@
 import { AppSidebar } from "@/components/app-sidebar"
 import { EventspaceCard } from "../eventspaces/eventspacecard";
 import { EventSpaces } from "@/app/authentication/eventspaces/eventspace";
+import Image from "next/image"
+import { Calendar } from "@/components/ui/calendar"
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
+import { Textarea } from "@/components/ui/textarea"
+import { Separator } from "@/components/ui/separator"
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -8,7 +14,16 @@ import {
   BreadcrumbList,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb"
-import { Separator } from "@/components/ui/separator"
+
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 import {
   SidebarInset,
   SidebarProvider,
@@ -41,6 +56,20 @@ export default function Page() {
           </div>
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4 pt-0 bg-[#EEF4ED]">
+            <div className="flex flex-col w-full max-w-5xl mx-auto mt-2 bg-[#dce5f2] border border-slate-400 rounded-xl shadow-sm overflow-hidden items-center justify-center ">
+                <div className="flex flex-col md:flex-row w-full h-full p-4 gap-6 justify-center items-center">
+                  <Card className="flex flex-col w-full md:flex-1 h-auto md:h-auto bg-[#556378] rounded-lg shadow-sm p-4 text-white justify-center items-center md:w-full">
+                    <Calendar
+                    mode="single"
+                    className="rounded-lg border bg-[#EEF4ED] text-[#556378] p-3 w-auto md:w-full md:h-full"
+                    />
+                  </Card>
+                  
+                </div>
+
+                <div className="p-6 flex flex-col gap-4">
+            </div>
+            </div>
         </div>
       </SidebarInset>
     </SidebarProvider>
