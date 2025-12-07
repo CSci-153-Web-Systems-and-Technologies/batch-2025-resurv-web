76723d01532bf646c6b9a265582085bad6cc1ae0
Gabriel Mosquera <mosqueragabriel99@gmail.com>
2025-12-07 16:44:09 +0800

    Change icon and title to match project icon

--- app/authentication/signup/page.tsx
+++ app/authentication/signup/page.tsx
@@ -1,17 +1,17 @@
 import { GalleryVerticalEnd } from "lucide-react"
-
+import { CalendarFold } from 'lucide-react';
 import { SignupForm } from "@/components/signup-form"
 
 export default function SignupPage() {
   return (
-    <div className="grid min-h-svh lg:grid-cols-2">
+    <div className="grid min-h-svh lg:grid-cols-2 bg-[#CEDBEE]">
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex justify-center gap-2 md:justify-start">
-          <a href="#" className="flex items-center gap-2 font-medium">
+          <a href="#" className=" font-extrabold tracking-tight flex items-center gap-2">
             <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-              <GalleryVerticalEnd className="size-4" />
+              <CalendarFold className="size-4" />
             </div>
-            Acme Inc.
+            Resurv
           </a>
         </div>
         <div className="flex flex-1 items-center justify-center">
