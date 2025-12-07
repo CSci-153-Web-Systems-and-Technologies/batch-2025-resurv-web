18f47445a8372d3032fd1fdc519e0e33f8cd5610
Gabriel Mosquera <mosqueragabriel99@gmail.com>
2025-12-07 20:33:13 +0800

    Add styling to Sidebar

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,9 +1,15 @@
 "use client"
 
 import * as React from "react"
+import { NavMain } from "@/components/nav-main"
+import { NavUser } from "@/components/nav-user"
+import { Button } from "./ui/button"
 import {
   AudioWaveform,
+  Calendar,
   BookOpen,
+  CalendarFold,
+  LayoutDashboard,
   Bot,
   Command,
   Frame,
@@ -14,8 +20,7 @@ import {
   SquareTerminal,
 } from "lucide-react"
 
-import { NavMain } from "@/components/nav-main"
-import { NavUser } from "@/components/nav-user"
+
 import {
   Sidebar,
   SidebarContent,
@@ -36,12 +41,12 @@ const data = {
     {
       title: "Dashboard",
       url: "#",
-      icon: SquareTerminal,
+      icon: LayoutDashboard
     },
     {
       title: "Calendar",
       url: "#",
-      icon: Bot,
+      icon: CalendarFold,
     },
     {
       title: "Contacts",
@@ -51,34 +56,36 @@ const data = {
     },
     
   ],
-  projects: [
-    {
-      name: "Design Engineering",
-      url: "#",
-      icon: Frame,
-    },
-    {
-      name: "Sales & Marketing",
-      url: "#",
-      icon: PieChart,
-    },
-    {
-      name: "Travel",
-      url: "#",
-      icon: Map,
-    },
-  ],
 }
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
-    <Sidebar collapsible="icon" {...props}>
-      <SidebarHeader>
+    <Sidebar collapsible="none" className="flex flex-col min-h-screen items-center justify-center bg-[#556378] "{...props}>
+      <SidebarHeader className="w-full px-2 py-4 h-32 flex items-center justify-center bg-[#556378] ">
+        <Button className="w-full justify-center py-2 gap-1 bg-transparent hover:bg-transparent">
+          <a
+            href="/authentication/dashboard"
+            className="flex items-center gap-2 justify-center w-full" 
+          >
+            <span
+              className={
+                "ml-2 text-4xl font-bold text-black transition-all duration-200 ease-in-out justify-center "
+                
+              }
+            >
+              <div className="w-6 h-6 flex items-center justify-center gap-1 text-[#C1E1C1]">
+                <CalendarFold size={500} className="text-[#C1E1C1] w-8! h-8!" />
+                 Resurv
+              </div>
+
+            </span>
+          </a>
+        </Button>
       </SidebarHeader>
-      <SidebarContent>
-        <NavMain items={data.navMain} />
+      <SidebarContent className= "flex-1 overflow-auto flex flex-col items-center px-2 w-full h-full text-4xl bg-[#556378] text-[#C1E1C1] ">
+        <NavMain items={data.navMain}  />
       </SidebarContent>
-      <SidebarFooter>
+      <SidebarFooter className= "mt-auto w-full px-2  bg-[#EEF4ED] hover:bg-white border-2 border-[#556378] rounded-sm">
         <NavUser user={data.user} />
       </SidebarFooter>
       <SidebarRail />
--- components/nav-main.tsx
+++ components/nav-main.tsx
@@ -1,5 +1,5 @@
 "use client"
-
+import Link from "next/link"
 import { ChevronRight, type LucideIcon } from "lucide-react"
 
 import {
@@ -27,45 +27,36 @@ export function NavMain({
     icon?: LucideIcon
     isActive?: boolean
     items?: {
-      title: string
-      url: string
+    title: string
+    url: string
     }[]
   }[]
 }) {
   return (
     <SidebarGroup>
-      <SidebarGroupLabel>Platform</SidebarGroupLabel>
       <SidebarMenu>
         {items.map((item) => (
-          <Collapsible
-            key={item.title}
-            asChild
-            defaultOpen={item.isActive}
-            className="group/collapsible"
-          >
-            <SidebarMenuItem>
-              <CollapsibleTrigger asChild>
-                <SidebarMenuButton tooltip={item.title}>
-                  {item.icon && <item.icon />}
-                  <span>{item.title}</span>
-                  <ChevronRight className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
-                </SidebarMenuButton>
-              </CollapsibleTrigger>
-              <CollapsibleContent>
-                <SidebarMenuSub>
-                  {item.items?.map((subItem) => (
-                    <SidebarMenuSubItem key={subItem.title}>
-                      <SidebarMenuSubButton asChild>
-                        <a href={subItem.url}>
-                          <span>{subItem.title}</span>
-                        </a>
-                      </SidebarMenuSubButton>
-                    </SidebarMenuSubItem>
-                  ))}
-                </SidebarMenuSub>
-              </CollapsibleContent>
-            </SidebarMenuItem>
-          </Collapsible>
+          <SidebarMenuItem key={item.title} className="flex w-full mb-2 items-center justify-center">
+            <SidebarMenuButton asChild tooltip={item.title}>
+              <Link
+                href={item.url}
+                className="
+                  flex items-center gap-3 w-full px-3 py-2
+                  rounded-md
+                  transition-colors duration-150
+                  border border-transparent
+                  hover:bg-[#EEF4ED] hover:text-[#556378]
+                "
+              >
+                {item.icon && (
+                  <div className="w-8 h-8 flex items-center justify-center shrink-0">
+                    <item.icon size={24} className="text-[#C1E1C1]" />
+                  </div>
+                )}
+                <span className="text-xl font-medium">{item.title}</span>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
         ))}
       </SidebarMenu>
     </SidebarGroup>
