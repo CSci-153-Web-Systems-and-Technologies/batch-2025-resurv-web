02eff28b0a20c768c2a9a8825070628649bfcd10
Gabriel Mosquera <mosqueragabriel99@gmail.com>
2025-12-07 21:08:23 +0800

    Added EventSpace cards to the dashboard
    Allows users to view the event spaces

--- app/authentication/dashboard/page.tsx
+++ app/authentication/dashboard/page.tsx
@@ -1,4 +1,5 @@
 import { AppSidebar } from "@/components/app-sidebar"
+import Image from "next/image"
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -13,6 +14,15 @@ import {
   SidebarProvider,
   SidebarTrigger,
 } from "@/components/ui/sidebar"
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
 export default function Page() {
   return (
@@ -40,12 +50,48 @@ export default function Page() {
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4 pt-0 bg-[#EEF4ED]">
           <div className="grid auto-rows-min gap-4 md:grid-cols-3 rows-3 p-3">
-            <div className="w-full h-full bg-black aspect-video"> test</div>
-            <div className="w-full h-full bg-black aspect-video"> test</div>
-            <div className="w-full h-full bg-black aspect-video"> test</div>
-            <div className="w-full h-full bg-black aspect-video"> test</div>
-            <div className="w-full h-full bg-black aspect-video"> test</div>
-            <div className="w-full h-full bg-black aspect-video"> test</div>
+           
+            <Card className="bg-[#556378] flex-col w-full h-full items-center justify-center flex rounded-xl px-4 pb-4 pt-0 hover:bg-[#4a5568] relative">
+                <CardHeader className="w-full h-55 absolute top-0 p-0! rounded-t-xl overflow-hidden">
+                <Image
+                src="/Convention.jpg"
+                alt="conventionimage"
+                fill
+                className="rounded-xl object-cover" 
+                />
+                </CardHeader>
+                <CardContent className="text-white font-bold text-2xl pt-55">
+                  Convention Center
+                </CardContent>
+
+              </Card>
+               <Card className="bg-[#556378] flex-col w-full h-full items-center justify-center flex rounded-xl px-4 pb-4 pt-0 hover:bg-[#4a5568] relative">
+                <CardHeader className="w-full h-55 absolute top-0 p-0! rounded-t-xl overflow-hidden">
+                <Image
+                src="/CCE.jpg"
+                alt="cceimage"
+                fill
+                className="rounded-xl object-cover" 
+                />
+                </CardHeader>
+                <CardContent className="text-white font-bold text-2xl pt-55 text-center">
+                  Center for Continuing Education
+                </CardContent>
+              </Card>
+
+              <Card className="bg-[#556378] flex-col w-full h-full items-center justify-center flex rounded-xl px-4 pb-4 pt-0 hover:bg-[#4a5568] relative">
+                <CardHeader className="w-full h-55 absolute top-0 p-0! rounded-t-xl overflow-hidden">
+                <Image
+                src="/Beach_Resort.jpg"
+                alt="beachresortimage"
+                fill
+                className="rounded-xl object-cover" 
+                />
+                </CardHeader>
+                <CardContent className="text-white font-bold text-2xl pt-55 text-center">
+                  Beach Garden
+                </CardContent>
+              </Card>
           </div>
         </div>
       </SidebarInset>
