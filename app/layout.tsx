f06a393ede4fbee27758685636e1bf674d4e00ce
Gabriel Mosquera <mosqueragabriel99@gmail.com>
2025-12-13 20:07:33 +0800

    Update layout file to support Clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import { AuthAlert } from "./checkrole/auth-alert";
 import { Suspense } from "react"; // 1. Import Suspense
-
+import { ClerkProvider } from "@clerk/nextjs";
+import "./globals.css";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body>
         <Suspense fallback={null}>
@@ -12,5 +14,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         {children}
       </body>
     </html>
+  </ClerkProvider>
   );
 }
