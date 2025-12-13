b38fdfe7fec115f51b3b4be69ae9122c6ae0d987
Gabriel Mosquera <mosqueragabriel99@gmail.com>
2025-12-13 21:15:13 +0800

    Update header styling in admin interface

--- app/admin/blocked/page.tsx
+++ app/admin/blocked/page.tsx
@@ -33,12 +33,11 @@ export default async function ReservationPage() {
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2 bg-[#CEDBEE]">
           <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
             <Separator orientation="vertical" className="mr-2 h-4" />
             <Breadcrumb>
               <BreadcrumbList>
                 <BreadcrumbItem className="hidden md:block">
-                  <BreadcrumbLink href="" className="text-black">Blocked Dates</BreadcrumbLink>
+                  <BreadcrumbLink href="" className="font-bold text-[#556378] text-lg">Blocked Dates</BreadcrumbLink>
                 </BreadcrumbItem>
               </BreadcrumbList>
             </Breadcrumb>
