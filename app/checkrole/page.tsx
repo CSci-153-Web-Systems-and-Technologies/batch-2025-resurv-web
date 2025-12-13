12de24ce00dd5115a67dcb669a4807f848644fbc
Gabriel Mosquera <mosqueragabriel99@gmail.com>
2025-12-13 19:50:50 +0800

    Redirect students to student interface

--- app/checkrole/page.tsx
+++ app/checkrole/page.tsx
@@ -13,10 +13,12 @@ export default async function CheckRole() {
     redirect("/admin/dashboard");
   } 
   
-  // 2. If User is Student (or anyone else), send to Student Dashboard
-  // If this component was protecting an Admin route, this redirect implies access denied.
-  // We add the query param here so the destination knows to show an alert.
-  redirect("/authentication/dashboard?unauthorized=true");
+// 2. Student Redirect
+  if (user.publicMetadata?.role === "student") {
+    redirect("/student/dashboard");
+  }
+
+
 
   return null; 
 }
